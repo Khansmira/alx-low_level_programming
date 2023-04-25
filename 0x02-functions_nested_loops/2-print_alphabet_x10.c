fc6c691e39ae2d8af04b88cdc1f7a8d753084094
Khansmira <miriamakanwa@gmail.com>
2023-04-25 16:19:01 -0700

    prints lowercase alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all lowercase alphabets 10x
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
