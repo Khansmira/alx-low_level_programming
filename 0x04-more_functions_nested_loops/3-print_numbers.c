ce050e169b4652db7d04c5067f15250c46e21c0b
Khansmira <miriamakanwa@gmail.com>
2023-04-27 21:54:45 -0700

    Function prints numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - prints the numbers from 0 to 9,
@@ -10,7 +11,7 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
