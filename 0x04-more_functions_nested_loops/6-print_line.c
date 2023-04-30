aba10a89be64f7fd538b3e65bd7331abaf6253b3
Khansmira <miriamakanwa@gmail.com>
2023-04-30 03:08:37 -0700

    function prints a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,19 +2,21 @@
 
 /**
  * print_line - prints a straight line
+ *
  * @n: number of times the character prints
- * function prints a new line if @n is
- * 0 or less
  */
+
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('-');
 		}
