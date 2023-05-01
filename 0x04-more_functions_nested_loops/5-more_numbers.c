368f29c4d87bed2b443c560e082506f616c3289d
Khansmira <miriamakanwa@gmail.com>
2023-04-30 19:19:30 -0700

    prints 10x the numbers 0 t0 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,6 +3,8 @@
 /**
  * more_numbers - prints 10x the numbers, from 0 to 14
  * followed by a new line
+ *
+ * Return: void
  */
 void more_numbers(void)
 {
@@ -13,8 +15,8 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j <= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
