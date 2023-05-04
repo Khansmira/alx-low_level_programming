7118c39a9e55892bba41cbdbf149dff591749ff7
Khansmira <miriamakanwa@gmail.com>
2023-05-04 16:10:40 -0700

    Capitalize First Word Of String

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _stcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
-char *cap_string(char *);
+char *cap_string(char *str);
 int _putchar(char c);
 char *leet(char *src);
 
