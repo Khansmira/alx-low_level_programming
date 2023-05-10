89eb55193734a6b2aa40b4f71f2e9d43309a7b67
Khansmira <miriamakanwa@gmail.com>
2023-05-10 07:31:23 -0700

    function prints sum of 2 diagonal matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,8 +13,11 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 = *(a + i * size + i);
-		sum2 = *(a + (size - 1 - i) * size + i);
+		sum1 += *(a + i * size + i);
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum2 += *(a + (size - 1 - i) * size + i);
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
