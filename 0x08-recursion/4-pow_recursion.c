574a381f02706cbe4b431d7f81cec8357da3be7b
Khansmira <miriamakanwa@gmail.com>
2023-05-11 21:40:42 -0700

    function returns number raised tothe power of

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,10 +14,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if (y == 0)
+       	if (y == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else
-		return (x * _pow_recursion(x, (y - 1)));
+		return (x * _pow_recursion(x, y - 1));
 }
