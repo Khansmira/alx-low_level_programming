ca1ba1918b943a94e11c85ecc3c86bdbdf4a6ea4
Khansmira <miriamakanwa@gmail.com>
2023-05-22 19:45:55 -0700

    prints a dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog struct
+ * @d: dog function
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if ((*d).name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (!d->age)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", (*d).age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
