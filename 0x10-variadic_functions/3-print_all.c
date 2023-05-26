5cdc75b17b70c161f0873da1d09b18953b254d9b
Khansmira <miriamakanwa@gmail.com>
2023-05-25 22:37:41 -0700

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,15 +33,18 @@ void print_int(va_list arg)
 
 void print_float(va_list arg)
 {
-	float f = va_arg(arg, float);
+	float f = va_arg(arg, double);
 
 	printf("%f", f);
 }
 
-
+/**
+ * print_string - function prints strings.
+ * @arg: third argument
+ */
 void print_string(va_list arg)
 {
-	char *s  = va_arg(arg, char *);
+	char *s = va_arg(arg, char *);
 
 	if (s == NULL)
 	{
@@ -61,4 +64,35 @@ void print_all(const char * const format, ...)
 {
 	va_list arg;
 	int i = 0;
-	char
+	int check;
+	char *septr = "";
+
+	print data[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(arg, format);
+
+	while (format && format[i])
+	{
+		check = 0;
+		while (check < 4 && format[i] != *(data[check].str))
+		{
+			check++;
+		}
+
+		if (check < 4)
+		{
+			printf("%s", septr);
+			data[check].print_all(arg);
+			septr = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(arg);
+}
