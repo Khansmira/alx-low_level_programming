3b673db83ca79af1dc5f81ee46fb02e4b8b72edd
khansmira <miriamakanwa@gmail.com>
2023-06-01 13:37:02 -0700

    prints elementsin a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list 
+ * @h : pointer to the head of the list
+ *
+ * Return: the size of elements in the node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t d = 0;
+
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d %s\n", h->len, h->str);
+		}
+		h = h->next;
+		d++;
+	}
+	return (d);
+}
