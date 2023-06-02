0c4b028c041b0a9021a66bf16d832ca5186963b0
khansmira <miriamakanwa@gmail.com>
2023-06-02 11:50:27 -0700

    prints before main func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+include "lists.h"
+
+/**
+ * bm_func - printsan input  before before main function is executed
+ */
+
+void bm_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
