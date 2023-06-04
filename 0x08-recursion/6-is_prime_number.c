4bc9b67428b78679deefc55542b15dea70ada2dc
khansmira <miriamakanwa@gmail.com>
2023-06-04 12:10:32 -0700

    new task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
+int _prime(int n, int i);
 /**
- * is_prime_number - Entry Function to check prime number
+ * is_prime_number - entry function
  * @n: integer
  * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	if (n < 2)
-		return (0);
 	return (_prime(n, 2));
 }
-
 /**
- * _prime  - Entry Function to check for prime
+ * _prime  - check for prime number 
  * @n: integer
  * @i: integer
  * Return: 1 if n is a prime number, 0 if not
  */
 int _prime(int n, int i)
 {
-	if (i = 1)
+	if (i >= n && n >= 2)
 		return (1);
-	else if (n % i == 0 || n < 0)
+	else if (n % i == 0 || n < 2)
 		return (0);
 	else
 		return (_prime(n, i + 1));
