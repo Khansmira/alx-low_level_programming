560f00367e7f477ad5730150e06907ac9a6a2826
khansmira <miriamakanwa@gmail.com>
2023-06-13 21:08:40 -0700

    file update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 
-	for (len = 0; text_content[len]; len++);
+	for (len = 0; text_content[len]; len++)
 
 
 	written = write(fdp, text_content, len);
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to the file
+ * @filename: name of file to append text to
+ * @text_content: text string to append
+ *
+ * Return: 1 on success or -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t written = 0;
+	int fdp;
+	size_t len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fdp = open(filename, O_WRONLY | O_APPEND);
+	if (fdp == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+
+		written = write(fdp, text_content, len);
+	}
+	close(fdp);
+	if (written == -1)
+		return (-1);
+	return (1);
+}
