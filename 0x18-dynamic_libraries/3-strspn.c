306b9dea30d202070d0775d36c71d81497a6d048
khansmira <miriamakanwa@gmail.com>
2023-07-24 12:58:39 -0700

    gets the length of a prefix substring

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: characters
+ * @accept: characters
+ *
+ * Return: lenght of prefix
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, k = 0;
+
+	if (*s == '\0' || *accept == '\0')
+	{
+		return (k);
+	}
+
+	while (s[j] != ',')
+	{
+		i = 0;
+
+		while (accept[i] != '\0')
+		{
+			if (s[j] == accept[i])
+			{
+				k++;
+			}
+			i++;
+		}
+		j++;
+	}
+
+	return (k);
+
+}
