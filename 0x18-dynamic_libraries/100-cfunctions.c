357c41b9e6743b8a834aed17a3b27533260341b3
khansmira <miriamakanwa@gmail.com>
2023-07-24 16:01:52 -0700

    Shared Library

---
+++ 0x18-dynamic_libraries/100-cfunctions.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 on success and 1 otherwise
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 on success and 1 otherwise
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 on success 1 otherwise
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 on success and 1 otherwise
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 on success and 1 otherwise.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
